0556b7f562cccbd8aa73c75f5d51f6492eb84ef8
Rich Harris <richard.a.harris@gmail.com>
2015-06-16 16:27:37 -0400

    allow options.dest to be omitted

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var path = require( 'path' );
 var r = require( 'rollup' );
 
 module.exports = function rollup ( inputdir, outputdir, options ) {
-	if ( !options.entry || !options.dest ) {
-		throw new Error( 'You must supply `entry` and `dest` options' );
+	if ( !options.entry ) {
+		throw new Error( 'You must supply `options.entry`' );
 	}
 
+	options.dest = path.resolve( outputdir, options.dest || options.entry );
 	options.entry = path.resolve( inputdir, options.entry );
-	options.dest = path.resolve( outputdir, options.dest );
 
 	return r.rollup( options ).then( function ( bundle ) {
 		return bundle.write( options );
-	})
+	});
 };
